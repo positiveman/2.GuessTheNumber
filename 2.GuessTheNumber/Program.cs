4abf4d4c1b7b6e5b3fe852323d1679591d66c77e
Oleg <avdeevoleg91@gmail.com>
2017-12-18 23:04:41 +0200

    v.1

--- 2.GuessTheNumber/Program.cs
+++ 2.GuessTheNumber/Program.cs
@@ -1,12 +1,37 @@
 ﻿using System;
 
-namespace _2.GuessTheNumber
+namespace GuessTheNumber
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int number = 88;
+
+
+            while (true)
+            {
+                Console.Write("Guess the number:\n");
+                int guess = int.Parse(Console.ReadLine());
+                if (guess < number)
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.Write("Too low...");
+                }
+                else if (guess > number)
+                {
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.Write("Too high...");
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.Write("You're right!\n");
+                    break;
+                }
+
+
+            }
         }
     }
 }
