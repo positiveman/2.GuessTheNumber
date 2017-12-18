c1a108e9444b4cf7143a290cce30bda4a49930dd
Oleg <avdeevoleg91@gmail.com>
2017-12-18 23:04:07 +0200

    Add project files.

---
+++ 2.GuessTheNumber/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _2.GuessTheNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
